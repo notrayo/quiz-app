847818c259bbdf1368254f11108575088c24e4a4
notrayo <rayoleeroy@gmail.com>
2024-03-13 12:40:40 +0300

    done welcome screen UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,8 +5,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Home Screen')),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.purpleAccent])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // const SizedBox(
+              //   height: 70,
+              // ),
+              Image.asset('assets/quiz-logo.png'),
+              const Text(
+                'Welcome to my Quiz App',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 75, 43, 136),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12.5, horizontal: 15)),
+                child: const Text('Start Quiz'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
